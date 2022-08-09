926776b422ecf6e35901a44bce05beab3ab81a89
warengeorge <georgeomiebi@gmail.com>
2022-08-09 07:24:17 -0700

    modifying _printf.c

--- _printf.c
+++ _printf.c
@@ -1,82 +1,49 @@
 #include "main.h"
-#include <stdlib.h>
+#include <stdarg.h>
+#include <stddef.h>
 
-/**
- * check_for_specifiers - checks if there is a valid format specifier
- * @format: possible format specifier
- *
- * Return: pointer to valid function or NULL
- */
-static int (*check_for_specifiers(const char *format))(va_list)
-{
-	unsigned int i;
-	print_t p[] = {
-		{"c", print_c},
-		{"s", print_s},
-		{"i", print_i},
-		{"d", print_d},
-		{"u", print_u},
-		{"b", print_b},
-		{"o", print_o},
-		{"x", print_x},
-		{"X", print_X},
-		{"p", print_p},
-		{"S", print_S},
-		{"r", print_r},
-		{"R", print_R},
-		{NULL, NULL}
-	};
-
-	for (i = 0; p[i].t != NULL; i++)
-	{
-		if (*(p[i].t) == *format)
-		{
-			break;
-		}
-	}
-	return (p[i].f);
-}
-
-/**
- * _printf - prints anything
- * @format: list of argument types passed to the function
- *
- * Return: number of characters printed
- */
 int _printf(const char *format, ...)
 {
-	unsigned int i = 0, count = 0;
-	va_list valist;
-	int (*f)(va_list);
+	va_list args;
+	int i = 0, k = 0;
+	int n_displayed = 0;
+	char *str = NULL;
 
-	if (format == NULL)
-		return (-1);
-	va_start(valist, format);
-	while (format[i])
+	va_start(args, format);
+
+	while (format[i] != '\0')
 	{
-		for (; format[i] != '%' && format[i]; i++)
-		{
-			_putchar(format[i]);
-			count++;
-		}
-		if (!format[i])
-			return (count);
-		f = check_for_specifiers(&format[i + 1]);
-		if (f != NULL)
-		{
-			count += f(valist);
-			i += 2;
-			continue;
-		}
-		if (!format[i + 1])
-			return (-1);
+		if (format[i] != '%')
 		_putchar(format[i]);
-		count++;
-		if (format[i + 1] == '%')
-			i += 2;
 		else
+		{
+			if (format[i + 1] == 'c')
+			{
+				_putchar(va_arg(args, int));
+				i++;
+			}
+			else if (format[i + 1] == 's')
+			{
+				i++;
+				str = va_arg(args, char *);
+				k = 0;
+				while (str[k] != '\0')
+				{
+					_putchar(str[k]);
+					k++;
+				}
+			}
+			else if (format[i + 1] == '%')
+			{
+				i++;
+				_putchar('%');
+				n_displayed++;
+			}
+		}
 			i++;
 	}
-	va_end(valist);
-	return (count);
+
+	va_end(args);
+
+	return (n_displayed);
 }
