c477ec9b026111cb83655accad579d54e4efbd58
John Iluyemi <johniluyemi@gmail.com>
2022-08-10 00:06:44 +0000

    trial

--- function_array.c
+++ function_array.c
@@ -9,28 +9,26 @@
 
 int printIdentifiers(char q, va_list arg)
 {
-        int Index;
-
-        struct array function[] = {
-                {"c", print_char},
-                {"s", print_string},
-                {"d", print_int},
-                {"i", print_int},
-                {"u", print_unsigned},
-                {"b", print_unsignedToBinary},
-                {"o", print_oct},
-                {"x", print_hex},
-                {"X", print_HEX},
-                {"S", print_String},
-                {NULL, NULL}
-i        };
-
-        for (Index = 0; function[Index].indentifier != NULL; Index++)
-        {
-                if (function[Index].indentifier[0] == q)
-                        return (function[Index].printer(arg));
-        }
-        return (0);
+int Index;
+struct array function[] = {
+{"c", print_char},
+{"s", print_string},
+{"d", print_int},
+{"i", print_int},
+{"u", print_unsigned},
+{"b", print_unsignedToBinary},
+{"o", print_oct},
+{"x", print_hex},
+{"X", print_HEX},
+{"S", print_String},
+{NULL, NULL}
+};
+for (Index = 0; function[Index].indentifier != NULL; Index++)
+{
+if (function[Index].indentifier[0] == q)
+return (function[Index].printer(arg));
+}
+return (0);
 }
 
 
