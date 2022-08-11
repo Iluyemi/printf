24c80d4e30d48bf4e201ee68d1cedebf7309f721
John Iluyemi <johniluyemi@gmail.com>
2022-08-11 10:22:23 +0000

    betty

--- test/main.c
+++ test/main.c
@@ -7,6 +7,7 @@
  *
  * Return: Always 0
  */
+
 int main(void)
 {
     int len;
@@ -38,6 +39,5 @@ int main(void)
     len2 = printf("Percent:[%%]\n");
     _printf("Len:[%d]\n", len);
     printf("Len:[%d]\n", len2);
-    _printf("Unknown:[%r]\n");
     return (0);
 }
